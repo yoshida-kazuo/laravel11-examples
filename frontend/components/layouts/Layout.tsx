a4b13360a43b2ea7d9cb041bc92d3df0c23e9da5
KazuoYoshida <vril.dev@gmail.com>
2025-01-05 10:43:54 +0900

    frontend:レイアウト調整

--- frontend/components/layouts/Layout.tsx
+++ frontend/components/layouts/Layout.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Head from 'next/head';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
+import { LayoutProps } from '../../types/layout';
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({
+    children,
+    title = 'Default Title',
+    description = 'Default description for the site.',
+    keywords = 'default, keywords, site',
+}) => {
     return (
         <>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+            </Head>
             <Header />
             <main>{children}</main>
             <Footer />
---
+++ frontend/types/layout.d.ts
@@ -0,0 +1,6 @@
+export interface LayoutProps {
+    children: React.ReactNode;
+    title?: string;
+    description?: string;
+    keywords?: string;
+}
